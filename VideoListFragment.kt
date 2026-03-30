174900e73c6e7c6b262ed85889c52f347f338335
samzakelosdzimande-cell <samzakelosdzimande@gmail.com>
2026-03-31 01:26:29 +0200

    Initial Android streaming app setup with Kotlin

---
+++ VideoListFragment.kt
@@ -0,0 +1,12 @@
+package com.example.streamingapp
+
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class VideoListFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_video_list, container, false)
+    }
+}
